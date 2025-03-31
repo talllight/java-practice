4fb24a126dfdbba861bba9a78c7fb476ad0fcec4
talllight <74751884+talllight@users.noreply.github.com>
2025-03-31 14:05:54 +0900

    Add files via upload

---
+++ Calc/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int num1, num2, num3;
+
+        System.out.print("숫자1를 입력하시오: ");
+        num1 = sc.nextInt();
+
+        System.out.print("숫자2를 입력하시오: ");
+        num2 = sc.nextInt();
+
+        System.out.print("숫자3를 입력하시오: ");
+        num3 = sc.nextInt();
+
+        System.out.println(num1-num2-num3);
+    }
+}
